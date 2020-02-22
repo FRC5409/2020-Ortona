29afa4465be5b90c8f66dab2b7f815445a5faa8a
karina <1vermakar@hdsb.ca>
2020-02-22 08:28:34 -0800

    got ride of unused import

--- src/main/java/org/frc/team5409/robot/commands/DriveCommand.java
+++ src/main/java/org/frc/team5409/robot/commands/DriveCommand.java
@@ -9,7 +9,7 @@ package org.frc.team5409.robot.commands;
 
 import edu.wpi.first.wpilibj.DriverStation;
 //import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.CommandBase;
