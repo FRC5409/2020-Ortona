516e7179d18dfa82bea2f0fff8b4dde7ef7a8505
Keith Davies <keithdaviesjr81@gmail.com>
2020-02-22 19:14:49 -0500

    Clean

--- src/main/java/org/frc/team5409/robot/util/Watchdog.java
+++ src/main/java/org/frc/team5409/robot/util/Watchdog.java
@@ -9,13 +9,15 @@ import edu.wpi.first.wpilibj.Timer;
  * <p> Watchdogs can be used to monitor objects that must 
  * be updated and checked frequently. For example, 
  * a motor controller may use a watchdog in order 
- * to ensure that the motor is update frequently enough. </p> 
+ * to ensure that the motor is updated frequently enough. </p> 
  * 
  * @author Keith Davies
  */
 public final class Watchdog {
     private final double m_expiry_time;
-    private double       m_timer, m_time_last;
+
+    private       double m_timer,
+                         m_time_last;
 
     /**
      * Constructs a Watchdog.
@@ -38,7 +40,7 @@ public final class Watchdog {
     } 
 
     /**
-     * Feeds the watchdog timer.
+     * Feeds / resets the watchdog timer.
      */
     public void feed() {
         m_timer = 0;
