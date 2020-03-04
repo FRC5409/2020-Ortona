d172f2aa2b91e2bb2804d3481c4eb8a72734884b
GoroAkechi99 <GWSBrooks-Prenger08-ICS2O1-01@hdsb.ca>
2020-03-04 17:09:51 -0500

    Fixed error in reverse intake

--- src/main/java/org/frc/team5409/robot/commands/ReverseIntake.java
+++ src/main/java/org/frc/team5409/robot/commands/ReverseIntake.java
@@ -35,7 +35,7 @@ public class ReverseIntake extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    subsys_Intake.retract();
+    subsys_Intake.intakeOn(-1);
   }
 
   // Returns true when the command should end.
