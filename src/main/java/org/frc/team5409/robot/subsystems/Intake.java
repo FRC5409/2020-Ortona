5edeed5b0ea41fe1080f957267c8c7e34b0cadc0
karina <1vermakar@hdsb.ca>
2020-03-05 17:07:06 -0500

    Intake jammed true/false

--- src/main/java/org/frc/team5409/robot/subsystems/Intake.java
+++ src/main/java/org/frc/team5409/robot/subsystems/Intake.java
@@ -7,9 +7,15 @@
 
 package org.frc.team5409.robot.subsystems;
 
+<<<<<<< HEAD
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
+=======
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+>>>>>>> Intake jammed true/false
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMax;
@@ -17,6 +23,7 @@ import com.revrobotics.CANSparkMax;
 import org.frc.team5409.robot.Constants;
 
 public class Intake extends SubsystemBase {
+<<<<<<< HEAD
 	private CANSparkMax    mot_intake_sparkMax_C12;
 	private DoubleSolenoid dsl_rightIntakeSolenoid;
 	private DoubleSolenoid dsl_leftIntakeSolenoid;
@@ -87,4 +94,61 @@ public class Intake extends SubsystemBase {
 		mot_intake_sparkMax_C12.set(output);
 		
 	}
+=======
+  private CANSparkMax mot_intake_sparkMax_C12;
+  private final CANEncoder intakeEncoder;
+  // private DoubleSolenoid dsl_rightIntakeSolenoid;
+  // private DoubleSolenoid dsl_leftIntakeSolenoid;
+
+  /**
+   * Creates a new Intake.
+   */
+  public Intake() {
+    mot_intake_sparkMax_C12 = new CANSparkMax(Constants.Intake.kIntakeMotor, MotorType.kBrushless);
+    mot_intake_sparkMax_C12.setSmartCurrentLimit(20); 
+    mot_intake_sparkMax_C12.setIdleMode(IdleMode.kBrake);
+    mot_intake_sparkMax_C12.burnFlash();  
+    //dsl_rightIntakeSolenoid = new DoubleSolenoid(Constants.Intake.kRightIntakeSolenoid1, Constants.Intake.kRightIntakeSolenoid2);
+    //dsl_leftIntakeSolenoid = new DoubleSolenoid(Constants.Intake.kLeftIntakeSolenoid1, Constants.Intake.kLeftIntakeSolenoid2);
+    intakeEncoder = mot_intake_sparkMax_C12.getEncoder();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  /**
+   * Method to extend intake
+   */
+  public void extend(double output) {
+    // dsl_rightIntakeSolenoid.set(DoubleSolenoid.Value.kForward);
+    // dsl_leftIntakeSolenoid.set(DoubleSolenoid.Value.kForward);
+    mot_intake_sparkMax_C12.set(output);
+  }
+
+  /**
+   * Method to retract intake
+   */
+  public void retract() {
+    // dsl_rightIntakeSolenoid.set(DoubleSolenoid.Value.kReverse);
+    // dsl_leftIntakeSolenoid.set(DoubleSolenoid.Value.kReverse);
+    mot_intake_sparkMax_C12.set(0);
+  }
+  /**
+   * Method to reverse intake, in case of jamming
+   */
+  public void reverse() {
+    // dsl_rightIntakeSolenoid.set(DoubleSolenoid.Value.kForward);
+    // dsl_leftIntakeSolenoid.set(DoubleSolenoid.Value.kForward);
+    mot_intake_sparkMax_C12.set(-1);
+  }
+
+
+  public boolean isIntakeNotJammed(){
+
+
+if (intakeEncoder)
+  }
+>>>>>>> Intake jammed true/false
 }
